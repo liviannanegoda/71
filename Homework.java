fee28d51d973924bef86b4ff50d5233ac64b3ebe
liviannanegoda <135698720+liviannanegoda@users.noreply.github.com>
2025-07-08 20:09:41 +0300

    Create Homework.java

---
+++ Homework.java
@@ -0,0 +1,10 @@
+package com.github.javarushcommunity.jrtb.repository;
+
+import com.github.javarushcommunity.jrtb.repository.entity.TelegramUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+public interface TelegramUserRepository extends JpaRepository<TelegramUser, String> {
+   List<TelegramUser> findAllByActiveTrue();
+}
